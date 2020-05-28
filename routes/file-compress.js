dedaff732f9c89bcbfe8e1bb6cb3babd2762977d
manishjayan <manishjayan@gmail.com>
2020-05-28 16:17:21 +0100

    file-compress merged with dashboard

--- routes/file-compress.js
+++ routes/file-compress.js
@@ -1,16 +1,18 @@
 
 const express = require('express');
 const router = express.Router();
-var sess;
+
 const spawn = require("child_process").spawn;
 
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
-router.get('/', (req, res) => res.render('file-compress'));
+
+router.get('/',ensureAuthenticated,(req, res) => res.render('file-compress'));
 
 router.post('/',(req, res) => {
-  const { bucketname, filename } = req.body;
-  sess=req.session;
-  console.log(sess.email);
+  const filelink = req.body.image;
+  var filelist=filelink.split(".com/")
+  const filename=filelist.slice(-1)[0];
+  bucketname=req.user.bucketname;
   const pythonProcess = spawn('python',["../comp/app.py",bucketname,filename]);
   pythonProcess.stdout.on('data', function(data) {
     console.log(data.toString());
