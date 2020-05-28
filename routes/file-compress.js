2ec029939522c93b230e72adf34c4409a3d08397
manishjayan <manishjayan@gmail.com>
2020-05-28 15:18:49 +0100

    file-compress added

--- routes/file-compress.js
+++ routes/file-compress.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const router = express.Router();
-
+var sess;
 const spawn = require("child_process").spawn;
 
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
@@ -9,7 +9,8 @@ router.get('/', (req, res) => res.render('file-compress'));
 
 router.post('/',(req, res) => {
   const { bucketname, filename } = req.body;
-  
+  sess=req.session;
+  console.log(sess.email);
   const pythonProcess = spawn('python',["../comp/app.py",bucketname,filename]);
   pythonProcess.stdout.on('data', function(data) {
     console.log(data.toString());
